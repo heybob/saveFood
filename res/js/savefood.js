af529e177f9c66798ec6a1d08c106f9245377429
Bob Filiczkowski <bfiliczkowski@gmail.com>
2016-09-01 00:02:15 -0700

    Added firebase

--- res/js/savefood.js
+++ res/js/savefood.js
@@ -1,1 +1,12 @@
+ // Initialize Firebase
+  //Move to app js file.
+  var config = {
+    apiKey: "AIzaSyDkT1Ozv1WFOVPG18AJylS7rsIxj46fjZc",
+    authDomain: "save-food-cba1b.firebaseapp.com",
+    databaseURL: "https://save-food-cba1b.firebaseio.com",
+    storageBucket: "save-food-cba1b.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+// AngularJS App
 var saveFood = angular.module('saveFood', ["firebase"]);
