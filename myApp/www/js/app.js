152cb2843250b3355e365d512ccbbb6682b99a5e
heybob <bfiliczkowski@gmail.com>
2017-04-23 16:26:28 -0700

    Changed continer list view to mirror expiring list view.

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -408,9 +408,51 @@ function detailListCtrl($scope, $state, dataService, dateFormatterService){
   var vm = this;
 
   // Public
+  $scope.getReadableDate = getReadableDate;
   $scope.isExpired = dateFormatterService.isExpired;
   $scope.useItem = dataService.useItem;
+  $scope.removeItem = dataService.removeItem;
   $scope.trashItems = dataService.trashItems;
+  $scope.extendExp = extendExp;
+  $scope.getContainerName = dataService.getContainerName;
+  $scope.isItemSelected = isItemSelected;
+  $scope.selectItem = selectItem;
+  $scope.filterByContainerId = filterByContainerId;
+
+  function getReadableDate(date) {
+    return dateFormatterService.getReadableDate(date);
+  }
+  
+  function isItemSelected(item){
+    if($scope.selected && item.$id === $scope.selected.$id){
+      return true;
+    }
+    return false;
+  }
+  function selectItem(item) {
+    if($scope.selected && item.$id === $scope.selected.$id){
+      clearSelectedItem();
+    } else {
+      $scope.selected = item;
+    }
+  }
+
+  function clearSelectedItem(){
+    $scope.selected = null;
+  }
+
+  function filterByContainerId(item){
+    if(item.containerId === $scope.container.$id){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function extendExp(item){
+    dataService.extendExp(item);
+    clearSelectedItem();
+  }
 
   $scope.$on("$ionicView.beforeEnter", function() {
     $scope.container = $state.params.container;
